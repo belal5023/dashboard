3f186559976ef0e48709d502d764481051aef2e0
BELAL KHAN <belal.gub@gmail.com>
2018-07-25 10:03:24 +0600

    initial file uploads

---
+++ assets/js/active.js
@@ -0,0 +1,18 @@
+(function ($) {
+    "use strict";
+
+    
+    /*at document loading time*/ 
+    jQuery(document).ready(function ($) {
+        
+		
+    });
+    
+    
+    
+    /*at window loading time*/    
+    jQuery(window).load(function () {
+        
+    });
+    
+}(jQuery));
